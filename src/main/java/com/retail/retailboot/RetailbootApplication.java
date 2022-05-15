8b2c13c430726b9d224205f8baa7ae8fb14a8ca1
foxhound234 <morganlb@protonmail.com>
2022-05-15 11:57:27 +0200

    first commit

---
+++ src/main/java/com/retail/retailboot/RetailbootApplication.java
@@ -0,0 +1,13 @@
+package com.retail.retailboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retail/retailboot/RetailbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retail.retailboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
